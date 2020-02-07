44c9cbc745163f901d842c5d0ed3e2d16063a332
lajawanti <lajawanti.dhake@gmail.com>
2020-02-07 15:06:31 -0500

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+//import from npm
 import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello.......</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+    }
+  }
+
+  componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => {
+              console.log("users   : ",users)
+              this.setState({ monsters : users})
+           })
+  }
+
+  render() {
+    console.log("this.state.monsters   :  ",this.state.monsters)
+    return (
+      <div className="App">
+          {this.state.monsters.map(monster => (
+              <h1 key = {monster.id}>{ monster.name }</h1>
+          ))}
+      </div> 
+    );
+  }
 }
 
 export default App;
