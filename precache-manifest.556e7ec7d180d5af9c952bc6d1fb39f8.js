1f7794ff48f4588a33014f7d29242ce84385b815
lajawanti <lajawanti.dhake@gmail.com>
2020-02-17 18:48:14 -0500

    Updates

---
+++ precache-manifest.556e7ec7d180d5af9c952bc6d1fb39f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b6f59483a53102cc1b5336545a8455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae774033efbbf9a82ae3",
+    "url": "/static/css/main.d4f494fe.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae774033efbbf9a82ae3",
+    "url": "/static/js/main.33178de1.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
